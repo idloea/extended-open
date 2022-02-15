fc0695b3484d48b5f2279db42fef9c0d770a8529
IKER.DELOMAOSORIO <iker.delomaosorio@siemensgamesa.com>
2022-02-15 14:23:11 +0100

    Add test to read csv from OPEN original case

---
+++ building_case_study.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from src.read import read_open_csv_files
+
+
+data_path = "Data/Building/"
+photovoltaic_generation_data_file = "PVpu_1min_2014JAN.csv"
+photovoltaic_generation_in_per_unit = read_open_csv_files(path=data_path,
+                                                          csv_file=photovoltaic_generation_data_file)
+electric_load_data_file = "Loads_1min_2014JAN.csv"
+electric_load_data = read_open_csv_files(path=data_path, csv_file=electric_load_data_file)
+
+sum_of_photovoltaic_generation_in_per_unit = np.sum(photovoltaic_generation_in_per_unit, 1)
---
+++ tests/src/read/test_read.py
@@ -0,0 +1,17 @@
+import unittest
+
+import numpy as np
+
+from src.read import read_open_csv_files
+
+
+class Read(unittest.TestCase):
+
+    @staticmethod
+    def test_read_open_csv_files():
+        expected_result = [0.66400, 0.24700, 0.04800]
+
+        path = 'tests/src/read'
+        data = read_open_csv_files(path=path, csv_file='Loads_1min_2013JUN.csv')
+        result = data[0][0:3]  # Get a sample
+        np.testing.assert_almost_equal(expected_result, result)
