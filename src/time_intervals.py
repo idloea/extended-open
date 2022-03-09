22e40f8ad28f89cc814e13a2e1e78a421923a672
IKER.DELOMAOSORIO <iker.delomaosorio@siemensgamesa.com>
2022-03-09 08:26:31 +0100

    Refactor variable names

--- src/time_intervals.py
+++ src/time_intervals.py
@@ -1,3 +1,3 @@
-def get_number_of_time_intervals_per_day(time_series_hour_resolution: float) -> int:
+def get_number_of_time_intervals_per_day(time_series_resolution_in_hours: float) -> int:
     hours_per_day = 24
-    return int(hours_per_day / time_series_hour_resolution)
+    return int(hours_per_day / time_series_resolution_in_hours)
--- tests/src/test_time_intervals.py
+++ tests/src/test_time_intervals.py
@@ -6,6 +6,6 @@ class TimeIntervals(unittest.TestCase):
 
     def test_get_number_of_time_intervals_per_day(self):
         expected_result = 12
-        time_series_hour_resolution = 2
-        result = get_number_of_time_intervals_per_day(time_series_hour_resolution=time_series_hour_resolution)
+        time_series_resolution_in_hours = 2
+        result = get_number_of_time_intervals_per_day(time_series_resolution_in_hours=time_series_resolution_in_hours)
         self.assertEqual(expected_result, result)
