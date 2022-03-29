93ff37138c631c1fc5011b493bbc3f16da5425bf
IKER.DELOMAOSORIO <iker.delomaosorio@siemensgamesa.com>
2022-03-29 12:37:39 +0200

    Change to summer scenario to match be able to compare with the original OPEN code results

--- building_case_study.py
+++ building_case_study.py
@@ -7,15 +7,15 @@ import pandapower as pp
 
 # STEP 0: Load Data
 data_path = "Data/Building/"
-photovoltaic_generation_data_file = "PVpu_1min_2014JAN.csv"
+photovoltaic_generation_data_file = "PVpu_1min_2013JUN.csv"
 photovoltaic_generation_in_per_unit = read_open_csv_files(path=data_path,
                                                           csv_file=photovoltaic_generation_data_file)
-electric_load_data_file = "Loads_1min_2014JAN.csv"
+electric_load_data_file = "Loads_1min_2013JUN.csv"
 electric_loads = read_open_csv_files(path=data_path, csv_file=electric_load_data_file)
 
 # Photovoltaic generation
 sum_of_photovoltaic_generation_in_per_unit = np.sum(photovoltaic_generation_in_per_unit, 1)
-is_winter = True
+is_winter = False
 max_photovoltaic_generation_in_per_unit = np.max(sum_of_photovoltaic_generation_in_per_unit)
 photovoltaic_generation_per_unit = sum_of_photovoltaic_generation_in_per_unit / max_photovoltaic_generation_in_per_unit
 
