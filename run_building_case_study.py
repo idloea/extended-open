1f0c53fe0978b685771b80ba13264a744246c5bf
IkerDeLoma-Osorio <iker.de@utopusinsights.com>
2022-12-01 10:52:01 +0100

    Refactorings

--- run_building_case_study.py
+++ run_building_case_study.py
@@ -1,4 +1,4 @@
-from building_case_study import run
+from src.building_case_study import run
 
 if __name__ == "__main__":
     # Photovoltaic generation
@@ -97,18 +97,18 @@ if __name__ == "__main__":
 
     }
     cases_file_path = 'data/cases/pamplona/workday'
-    yaml_files = ['01_january_no_flexibility.yaml'
-                  # '02_february_no_flexibility.yaml',
-                  # '03_march_no_flexibility.yaml',
-                  # '04_april_no_flexibility.yaml',
-                  # '05_may_no_flexibility.yaml',
-                  # '06_june_no_flexibility.yaml',
-                  # '07_july_no_flexibility.yaml',
-                  # '08_august_no_flexibility.yaml',
-                  # '09_september_no_flexibility.yaml',
-                  # '10_october_no_flexibility.yaml',
-                  # '11_november_no_flexibility.yaml',
-                  # '12_december_no_flexibility.yaml'
+    yaml_files = ['01_january_no_flexibility.yaml',
+                  '02_february_no_flexibility.yaml',
+                  '03_march_no_flexibility.yaml',
+                  '04_april_no_flexibility.yaml',
+                  '05_may_no_flexibility.yaml',
+                  '06_june_no_flexibility.yaml',
+                  '07_july_no_flexibility.yaml',
+                  '08_august_no_flexibility.yaml',
+                  '09_september_no_flexibility.yaml',
+                  '10_october_no_flexibility.yaml',
+                  '11_november_no_flexibility.yaml',
+                  '12_december_no_flexibility.yaml'
                   ]
     results_path = 'results'
     run(cases_file_path=cases_file_path, yaml_files=yaml_files, general_case_data=general_case_data,
